5032e26f1bfe26961ef9c8e505857944c2949658
jshan <jshan0309@hotmail.com>
2024-08-13 14:28:24 +0800

    feat. test c1 ani

---
+++ src/js/index.js
@@ -0,0 +1,17 @@
+window.onload = function () {
+    var window_width = window.screen.width;
+    var window_height = window.innerHeight;
+    let vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty("--vh", `${vh}px`);
+    gsap.registerPlugin();
+
+    function c1Ani() {
+        let tl = gsap.timeline({});
+        tl.to(".card1", { maskSize: "400%", ease: Power0.easeNone, duration: 6, })
+            .from('.logo-box', { y: 100, opacity: 0, duration: 2, }, "<+0.8")
+            .from('.title-box', { y: 80, opacity: 0, duration: 1.5, }, "<+0.3")
+            .from('.arrow-box', { opacity: 0, duration: 1, ease: 'power0.in', }, "<0.5")
+            .from('.header-img', { opacity: 0, duration: 1, ease: 'power0.in', }, "<")
+    }
+    c1Ani()
+};
