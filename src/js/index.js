abd3b2ecf1bd563ed398975551fbb4df449bf167
jshan <jshan0309@hotmail.com>
2024-08-26 17:27:38 +0800

    feat. 首頁切版

--- src/js/index.js
+++ src/js/index.js
@@ -5,6 +5,34 @@ window.onload = function () {
     document.documentElement.style.setProperty("--vh", `${vh}px`);
     gsap.registerPlugin();
 
+    const c3swiper = new Swiper(".card3-swiper", {
+        loop: true,
+        speed: 1500,
+        effect: 'fade',
+        loop: true,
+        fadeEffect: {
+            crossFade: true,
+        },
+        pagination: {
+            el: '.c3-swiper-pagination',
+            clickable: true,
+        },
+    });
+
+    const c4swiper = new Swiper(".card4-swiper", {
+        loop: true,
+        speed: 1500,
+        effect: 'fade',
+        loop: true,
+        fadeEffect: {
+            crossFade: true,
+        },
+        pagination: {
+            el: '.c4-swiper-pagination',
+            clickable: true,
+        },
+    });
+
     function c1Ani() {
         let tl = gsap.timeline({});
         tl.to(".card1", { maskSize: "400%", ease: Power0.easeNone, duration: 6, })
@@ -13,5 +41,5 @@ window.onload = function () {
             .from('.arrow-box', { opacity: 0, duration: 1, ease: 'power0.in', }, "<0.5")
             .from('.header-img', { opacity: 0, duration: 1, ease: 'power0.in', }, "<")
     }
-    c1Ani()
+    // c1Ani()
 };
